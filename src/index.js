680a349cc5d6c7199aa76046cbce3a63efffe6fb
“mbuimbogo” <peter.mbogo@student.moringaschool.com>
2022-08-24 23:43:22 +0300

    Completed assignment

--- src/index.js
+++ src/index.js
@@ -1,3 +1,15 @@
 document.addEventListener("DOMContentLoaded", () => {
   // your code here
-});
+  let form = document.querySelector('form')
+  form.addEventListener('submit', (e) => {
+    e.preventDefault();
+    let li = document.createElement("li");
+    const userInput = document.getElementById('new-task-description');
+    li.innerText = userInput.value
+    let ul = document.getElementById("tasks");
+    ul.append(li);
+    const button = document.createElement("button");
+    button.innerText = "X"
+  })
+})
+
